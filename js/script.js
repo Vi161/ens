38ec14771014914055808155150fa985c0e3fb68
vladimir <orlovvalentino@gmail.com>
2017-09-19 22:31:58 +0300

    menu upgrade 8

--- js/script.js
+++ js/script.js
@@ -84,10 +84,9 @@ $(document).ready(function(){
 //        $('.slicknav-menu1__but').css("background","red")
     });
     $('.slicknav-menu1__but').click(function(){
-        $(".header-nav__wrap").toggleClass("header-nav__wrap__toggle");
         $(".icon-bars").toggleClass("icon-close");
-        $(".header-nav").toggleClass("header-nav__toggle");
         $( "#header-nav__list").slideToggle("slow");
+        return false;
     });
 });
 //        var mibile_width = 767;
