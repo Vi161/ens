da4be74166e97a176036059d296f2f7f9d34775a
vladimir <orlovvalentino@gmail.com>
2017-09-22 14:04:51 +0300

    menu2109-1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 //  ---------перелистывание к якорям-------------
 $(document).ready(function() {
-    $('a[href^="#"]').click(function(){
-        var el = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(el).offset().top}, 500);
-        return false;
-    });
+
+
 });
 
 
@@ -14,9 +10,10 @@ var navmenu = function(class1){
     var iScrollPos = 0;
     var hproductsnav= $(".products-nav").outerHeight();
     var hheadernav= $(".header-nav").outerHeight();
-
+    var paddingtop;
     var mibile_width = 767;
     if($(window).width() > mibile_width) {
+
         $(window).scroll(function () {
             var iCurScrollPos = $(this).scrollTop();
             if($(this).scrollTop() >= 55) {
@@ -27,32 +24,46 @@ var navmenu = function(class1){
                 $(class1).removeClass('nav-fixed');//rem2
                 $('header').removeClass('header-wrap-fixed');//add1
             }
+
             if ((iCurScrollPos < iScrollPos) &&  ($(this).scrollTop() >= 55) ) {
                 $('header').addClass('header-wrap-fixed');//add1
-                $(class1).css("top",hheadernav)
+                $(class1).css("top",hheadernav);
+                paddingtop = hproductsnav + hheadernav;
             }
             else if (iCurScrollPos > iScrollPos){
                 $('header').removeClass('header-wrap-fixed');//add1
+                paddingtop = hproductsnav;
             }
             var lastScrollTop = 0;
             iScrollPos = iCurScrollPos;
         });
+
     }else{
 
         $(window).scroll(function () {
             var iCurScrollPos = $(this).scrollTop();
+            paddingtop = hproductsnav;
+
             if($(this).scrollTop() >= 55) {
                 $(class1).addClass('nav-fixed'); //add2
             }
             else if ((iCurScrollPos < iScrollPos)  ) {
                 $(class1).removeClass('nav-fixed');//rem2
                 $('header').removeClass('header-wrap-fixed');//add1
-
             }
             var lastScrollTop = 0;
             iScrollPos = iCurScrollPos;
         });
     };
+    //  ---------перелистывание к якорям-------------
+
+    $('a[href^="#"]').click(function(){
+        var el = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(el).offset().top-paddingtop}, 500);
+            alert(paddingtop);
+        return false;
+    });
 };
 
 //var navmenu = function(class1){
